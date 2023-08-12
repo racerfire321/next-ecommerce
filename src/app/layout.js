7bbbd72c434b81957760a864c8b80b8ccde1eef0
racerfire321 <racerfire321@gmail.com>
2023-08-12 09:18:41 +0545

    head jspage

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      <head/>
       <body>
       <Header/>
         {children}</body>
